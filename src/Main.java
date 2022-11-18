145930bea90cdd7c20a56e9e01c2ade26f7936f9
Denis <protsdv@gmail.com>
2022-11-18 10:57:17 +0300

    Home Work is done! Brackets are added.

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,27 @@ public class Main {
         //////////////////////// Exercise 1 ////////////////////////
         boolean clientOS = true;
         System.out.println("Exercise 1");
-        if (clientOS) System.out.println("Установите версию приложения для Android по ссылке");
-        else System.out.println("Установите версию приложения для iOS по ссылке");
+        if (clientOS) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
 
         //////////////////////// Exercise 2 ////////////////////////
         int releaseYear = 2017;
         int oldRelease = 2015;
         System.out.println("\nExercise 2");
         if (clientOS) {
-            if (releaseYear >= oldRelease) System.out.println("Установите версию приложения для Android по ссылке");
-            else System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (releaseYear >= oldRelease) System.out.println("Установите версию приложения для iOS по ссылке");
-        else System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            if (releaseYear >= oldRelease) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else if (releaseYear >= oldRelease) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
 
         //////////////////////// Exercise 3 ////////////////////////
         int year = 2000;
@@ -33,10 +42,13 @@ public class Main {
         int rangeDistance = 40;
         int deliveryDays = 1 + (deliveryDistance - shortDistance) / rangeDistance;
         System.out.println("\nExercise 4");
-        if (deliveryDistance <= shortDistance) System.out.println("Потребуется дней: 1");
-        else if ((deliveryDistance - shortDistance) % rangeDistance == 0)
+        if (deliveryDistance <= shortDistance) {
+            System.out.println("Потребуется дней: 1");
+        } else if ((deliveryDistance - shortDistance) % rangeDistance == 0) {
             System.out.println("Потребуется дней: " + deliveryDays);
-        else System.out.println("Потребуется дней: " + (deliveryDays + 1));
+        } else {
+            System.out.println("Потребуется дней: " + (deliveryDays + 1));
+        }
 
         //////////////////////// Exercise 5 ////////////////////////
         int monthNumber = 11;
